74aee182fd8bf401f5f0341afb162111b0160b53
Kiorenc3 <johita12344779@gmail.com>
2026-02-24 12:43:02 -0500

    Estructura inicial carrito con testing

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+export default {
+  testEnvironment: "jsdom",
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
+};
